a6cadea2209989732dd31fcf0a9f8d5f3301412d
Nicolas Alvarez <nicolas210825gmail.com>
2025-04-03 19:14:22 -0500

    Se crean componentes

---
+++ hotel-app/src/App.jsx
@@ -0,0 +1,23 @@
+import About from "./components/about";
+import Banner from "./components/banner";
+import Contacto from "./components/contacto";
+import Footer from "./components/footer";
+import Habitaciones from "./components/habitaciones";
+import Menu from "./components/menu";
+import Ours from "./components/ours";
+
+function App() {
+  return (
+    <>
+     <Menu/>
+     <Banner/>
+     <Habitaciones/>
+     <About/>
+     <Ours/>
+     <Contacto/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
